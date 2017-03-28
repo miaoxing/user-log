516c1a18e288dd9fb9a8c162d5349a9e7c56fcbb
twinh <twinhuang@qq.com>
2017-03-28 16:34:10 +0800

    拆分用户日志菜单 #3323

--- src/Plugin.php
+++ src/Plugin.php
@@ -15,4 +15,20 @@ class Plugin extends BasePlugin
      * {@inheritdoc}
      */
     protected $description = '后台展示用户操作日志的列表';
+
+    public function onAdminNavGetNavs(&$navs, &$categories, &$subCategories)
+    {
+        $subCategories['user-log'] = [
+            'parentId' => 'user',
+            'name' => '日志',
+            'icon' => 'fa fa-bars',
+            'sort' => 0,
+        ];
+
+        $navs[] = [
+            'parentId' => 'user-log',
+            'url' => 'admin/user-logs',
+            'name' => '操作日志',
+        ];
+    }
 }
