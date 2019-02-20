7496c869116d6eab3d7d5f9133c3b6a50d4131fe
twinh <twinhuang@qq.com>
2019-02-20 11:23:09 +0800

    refactoring: data-table 迁移

--- resources/views/admin/userLogs/index.php
+++ resources/views/admin/userLogs/index.php
@@ -66,7 +66,7 @@
 
 <?= $block->js() ?>
 <script>
-  require(['dataTable', 'jquery-deparam', 'form', 'daterangepicker'], function () {
+  require(['plugins/admin/js/data-table', 'jquery-deparam', 'form', 'daterangepicker'], function () {
     var $recordTable = $('.js-log-table').dataTable({
       ajax: {
         url: $.queryUrl('admin/user-logs.json')
